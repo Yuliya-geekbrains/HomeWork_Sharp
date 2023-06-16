af6259811e97de1dba820c2a71edfc3d4a1c6498
Юлия <YUGG2512@gmail.com>
2023-06-16 23:19:44 +0300

    Homework

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу которая на вход принимает два числа и выдает какое число больше какое меньше
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if(a > b)
+{
+    Console.WriteLine("Max = a");
+}
+else
+{
+    Console.WriteLine("Max = b");
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N),а на выходе показывает все четные числа от 1 до N.
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+int min = 1;
+
+while(N >= min)
+
+{
+    if(min % 2 == 0)
+    {
+        Console.Write($"{min} ");
+        min++;
+    }
+    else min++;    
+}
+
+
+
